7496c1d2c675c2c694f880ef566669515696a0f3
Matt Stobo <mwstobo@gmail.com>
2016-08-10 19:51:32 -0400

    Remove create button from public procedures

--- src-backbone/app/js/views/procedures/publicProceduresLayoutView.js
+++ src-backbone/app/js/views/procedures/publicProceduresLayoutView.js
@@ -11,4 +11,15 @@ module.exports = ProceduresLayoutView.extend({
     initialize: function() {
         this.procedures = new PublicProcedures();
     },
+
+    events: {
+        'keyup @ui.procedureFilterInput': '_filterProcedures',
+
+        'click a#sort-by-title': '_changeSortKey',
+        'click a#sort-by-author': '_changeSortKey',
+        'click a#sort-by-last-modified': '_changeSortKey',
+
+        'click a#asc-order': '_changeSortOrder',
+        'click a#desc-order': '_changeSortOrder',
+    },
 });
