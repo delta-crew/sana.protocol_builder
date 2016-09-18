6f29f9ef7598e41dbdc484a738389815f6a59d61
Matt Stobo <mwstobo@gmail.com>
2016-09-18 17:04:27 -0400

    Frontend changes

--- src-backbone/app/js/collections/procedures.js
+++ src-backbone/app/js/collections/procedures.js
@@ -10,30 +10,8 @@ module.exports = Backbone.Collection.extend({
     constructor: function(models, options) {
         this.setAscOrder(false);
         this.setComparatorKey('last_modified');
-        this.comparator = this._comparator;
 
         Backbone.Collection.prototype.constructor.call(this, models, options);
-    },
-
-    _comparator: function(a, b) {
-        let ascComparator = function(l, r) {
-            // -1     if the first model should come before the second
-            //  0     if they are of the same rank
-            //  1     if the first model should come after
-
-            if (l === void 0) return -1;
-            if (r === void 0) return 1;
-
-            if (l < r) {
-                return -1;
-            } else if (l > r) {
-                return 1;
-            } else {
-                return 0;
-            }
-        };
-
-        return (this._isAsc ? 1 : -1) * ascComparator(a.get(this._comparatorKey), b.get(this._comparatorKey));
     },
 
     setComparatorKey: function(key) {
@@ -52,6 +30,10 @@ module.exports = Backbone.Collection.extend({
 
     getOrderName: function() {
         return (this._isAsc ? 'asc' : 'desc');
+    },
+
+    getOrderSymbol: function() {
+        return (this._isAsc ? '' : '-');
     },
 
 });
--- src-backbone/app/js/collections/publicProcedures.js
+++ src-backbone/app/js/collections/publicProcedures.js
@@ -1,5 +1,12 @@
 let Procedures = require('collections/procedures');
 
 module.exports = Procedures.extend({
-    url: '/api/procedures?is_public=true',
+    url: '/api/procedures?public=true',
+
+    constructor: function(models, options) {
+        this.setAscOrder(false);
+        this.setComparatorKey('fork_count');
+
+        Backbone.Collection.prototype.constructor.call(this, models, options);
+    },
 });
